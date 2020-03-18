04510e5ea178707cc8d189e3e9edb182fa3d9d73
Mario Varas <elasgard@hotmail.es>
2020-03-17 19:33:24 -0500

    (feat) CWE getters
    - typo

--- src/fakerCWEmodels.php
+++ src/fakerCWEmodels.php
@@ -6,7 +6,7 @@ class CWEData {
 
     protected static $cweId_models = array();
     protected static $cweDescription_models = array();
-    protected static $cweRecomendation_models = array();
+    protected static $cweRecommendation_models = array();
     protected static $cweRequirements_models = array();
     protected static $cweTitle_models = array();
     protected static $cweType_models = array();
@@ -18,11 +18,18 @@ class CWEData {
         foreach ($cwe_json as &$cwe) {
             array_push(static::$cweId_models, $cwe['cwe']);
             array_push(static::$cweDescription_models, $cwe['description']);
-            array_push(static::$cweRecomendation_models, $cwe['recommendation']);
+            array_push(static::$cweRecommendation_models, $cwe['recommendation']);
             array_push(static::$cweRequirements_models, $cwe['requirements']);
             array_push(static::$cweTitle_models, $cwe['title']);
             array_push(static::$cweType_models, $cwe['type']);
         }
     }
+
+    public static function getCWEId() : array { return static::$cweId_models; }
+    public static function getCWEDescription() : array { return static::$cweDescription_models; }
+    public static function getCWERecommendation() : array { return static::$cweRecommendation_models; }
+    public static function getCWERequirements() : array { return static::$cweRequirements_models; }
+    public static function getCWETitle() : array { return static::$cweTitle_models; }
+    public static function getCWEType() : array { return static::$cweType_models; }
 }
 ?>
